0a0a9efd4ecc031060a00f29828901d49a544dbc
gustavo de assis <gustavo.siqueira.421@gmail.com>
2023-02-01 09:37:12 -0300

    trying to find a worker at github

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app
   .use(cors())
   .use(express.json())
-  .get("/health", (_req, res) => res.send("OK!"))
+  .get("/health", (_req, res) => res.send("OK! "))
   .use("/users", usersRouter)
   .use("/auth", authenticationRouter)
   .use("/event", eventsRouter)
